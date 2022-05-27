c8a3a6e2052c1712e03c2ba2ad241fe9b505c43c
Parkdongkyu <qkrehdrb0813@gmail.com>
2022-05-27 18:38:36 +0900

    5-27 ~4.7 regex

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+
+const logger = morgan("dev");
+app.use(logger);
 // SANDWICH
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleHome = (req, res) => {
-  return res.send("i love middleware");
-};
-
-app.get("/", logger, handleHome);
 // SANDWICH
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
